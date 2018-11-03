2efb495f9d9c11963493184eab82f676cff5b7b6
Dana Palmer <roiai4@hotmail.com>
2018-11-03 01:43:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,7 +20,7 @@ class Triangle
 def triangle?
   if @a == 0 && @b == 0 && @c == 0 ||
     @a < 0 || @b < 0 || @c < 0 ||
-    @a + @b < @c || @a + @c < @b || @c + @b < @a
+    @a + @b <= @c || @a + @c <= @b || @c + @b <= @a
     raise TriangleError
   end
 end
